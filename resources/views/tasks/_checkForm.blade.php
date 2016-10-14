2a7fe18b2151843798bfd00797a9361134295481
gavin <ghcgavin@sina.com>
2016-10-14 14:43:57 +0800

    rm

--- resources/views/tasks/_checkForm.blade.php
+++
@@ -1,5 +0,0 @@
-{!! Form::open(['route'=>['tasks.check',$task->id],'method'=>'post']) !!}
-<button type="submit" class="btn btn-success btn-sm">
-    <i class="fa fa-btn fa-check"></i>
-</button>
-{!! Form::close() !!}
--- resources/views/tasks/_deleteForm.blade.php
+++
@@ -1,5 +0,0 @@
-{!! Form::open(['route'=>['tasks.destroy',$task->id],'method'=>'delete']) !!}
-<button type="submit" class="btn btn-danger btn-sm">
-    <i class="fa fa-close"></i>
-</button>
-{!! Form::close() !!}
